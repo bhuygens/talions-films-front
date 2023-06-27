216291c2c5749c64bbaa9f1395a0d53c9b5bd029
bhuygens <huygens.benjamin@gmail.com>
2023-06-27 22:06:21 +0200

    initial commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return <>
+    <section>
+      <div className="border">
+        <h1 className="text-center text-3xl">TALION FILMS</h1>
+      </div>
+    </section>
+  </>
+}
