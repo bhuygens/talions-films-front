4d5133418a5198f200bc7b9fc23f4751294ee8a6
bhuygens <huygens.benjamin@gmail.com>
2023-06-27 22:06:21 +0200

    initial commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return <>
+    <section>
+      <div className="border">
+        <p className="text-center text-3xl font-semibold">•TalionFilms reserved•</p>
+      </div>
+    </section>
+  </>;
+}
